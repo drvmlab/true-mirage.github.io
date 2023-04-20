bcb0bd75c6dd809046a3774875788fe9d7052f23
True Mirage <63160000+drvmlab@users.noreply.github.com>
2023-04-20 23:14:12 +0200

    Add files via upload

---
+++ truemirage.php
@@ -0,0 +1,35 @@
+<div class="section section1">
+   <a href="mid_journey.html">
+      <h2 class="visible">Mid journey</h2>
+   </a>
+   <div class="banner">
+      <a href="mid_journey.html">
+         <img src="images/midjourney1.png" alt="mid journey">
+      </a>
+   </div>
+   <p></p>
+</div>
+
+<div class="section section2">
+   <a href="music.html">
+      <h2 class="visible">Music</h2>
+   </a>
+   <div class="banner">
+      <a href="music.html">
+         <img src="images/music.png" alt="Music">
+      </a>
+   </div>
+   <p></p>
+</div>
+
+<div class="section section3">
+   <a href="video.html">
+      <h2 class="visible">Video</h2>
+   </a>
+   <div class="banner">
+      <a href="video.html">
+         <img src="images/video.png" alt="video">
+      </a>
+   </div>
+   <p></p>
+</div>
